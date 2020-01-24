e6efcfd9e8277568330a41f326af213aab71b700
Ali Rizvi <frontear4594@gmail.com>
2020-01-24 18:19:27 -0500

    Validate input arguments as existing directories
    This is pretty self explanatory.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().skip(1).collect(); // path argument not necessary
+
+    for arg in &args {
+        let path = Path::new(&arg);
+
+        if !path.is_dir() {
+            eprintln!("{} is not a valid directory (skipping)", path.display());
+        }
+    }
 }
