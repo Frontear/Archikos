ba68ed02316de5e17a02533fd65aecf3a2c32633
Ali Rizvi <frontear4594@gmail.com>
2020-01-24 18:51:31 -0500

    Change handling to filter the vector instead
    Also relatively self explanatory.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,21 @@ use std::env;
 use std::path::Path;
 
 fn main() {
-    let args: Vec<_> = env::args().skip(1).collect(); // path argument not necessary
+    let mut paths: Vec<_> = env::args()
+        .skip(1) // ignore binary path
+        .filter(|x| {
+            let path = Path::new(&x);
 
-    for arg in &args {
-        let path = Path::new(&arg);
+            if !path.is_dir() {
+                eprintln!("{} is not a valid directory (skipping)", path.display());
+                return false;
+            }
 
-        if !path.is_dir() {
-            eprintln!("{} is not a valid directory (skipping)", path.display());
-        }
-    }
+            return true;
+        })
+        .collect();
+
+    paths.sort();
+    paths.dedup(); // remove duplicate paths
+    println!("{:?}", paths);
 }
